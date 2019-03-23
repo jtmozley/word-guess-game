3769b306359f52292b0d0ab6c032016b4675f11a
JTMozley <jtmozley@gmail.com>
2019-03-22 19:58:34 -0500

    logic is about 90% complete with a bug on displaying the updated page after a user gets letters correct

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,93 @@
-//contains names to be used in game
+//names to be used in game
 var characters = ["elsa", "mickey", "ariel", "goofy", "woody"];
-//secret word
+//word thats selected from characters[]
 var secretWord = "";
-//user's array
+//user's array that stores "_" blanks initially
 var usersArray = [];
 //letters used
 var wrongWordArray = [];
+//puts selected word into array of strings
 var logged = "";
-// console.log(logged);
-
-var wins = 0;
-var losses = 0;
-var guessesLeft;
+//stores which index the userInput equals inside of the logged[]
+var indx;
+//key the user pressed
 var userInput = "";
-var isWordDone = false;
+//var isWordDone = false;
 
+//stats displayed to user
+var wins = 11110;
+var losses = 0;
+var guessesLeft = 0;
+
+var winsText = document.getElementById("wins");
+var lossesText = document.getElementById("losses");
+var currentWord = document.getElementById("currentWord");
+var guessesLeftText = document.getElementById("guessesLeft");
+var lettersUsed = document.getElementById("lettersUsed");
+
+//updates any page displayed stats
+function  updateData() {
+	winsText.innerHTML = wins;
+	lossesText.innerHTML = losses;
+	currentWord.innerHTML = usersArray.join(" ");
+	guessesLeftText.innerHTML = guessesLeft;
+	lettersUsed.innerHTML = wrongWordArray;
+};
+
+
+//initializes the game and calles for a new word
 function start() {
   wins = 0;
   losses = 0;
-  guessesLeft = 15;
-  getSecretWord();
+	guessesLeft = 15;
+	updateData();
+	getSecretWord();
+
 }
 
 start();
 
+//select new word, adds to logged, replaces letters with "_" and pushes to usersArray[]
 function getSecretWord() {
   secretWord = characters[Math.floor(Math.random() * characters.length)];
   logged = secretWord.split("");
   for (var x = 0; x < logged.length; x++) {
     usersArray.push("_");
-  }
+	}
+	updateData();
   console.log(secretWord);
   console.log(logged);
   console.log(usersArray);
 
-  return usersArray;
+  //return usersArray;
 }
 
-// *on key press event*
-// -if the guesses left is greater than 0 THEN check to see if letter pressed matches a letter in the word
-// --if the letter matches a letter in the world then insert the letter into the array at the spot where
-//		the letter(s) are supposed to be and subtract a guess
-// --if NOT then log the word in the wrong word list and subtract a guesses left
-// -if the guesses left is less than or equal to 0 THEN the game is over and the losses will update by 1,
-//		the game will need to restart, and guesses left will need to be changed back to 15, and a new world
-//		will need to be generated
+
+//records input from user
 document.onkeyup = function(event) {
 	userInput = event.key.toLowerCase();
 	console.log(userInput);
-
-  if (userInput) {
-    for (var i = 0; i < logged.length; i++) {
-      if (userInput == logged[i]) {
-				usersArray[i] = logged[i];
-				guessesLeft--;
-      } else {
-        wrongWordArray.push(userInput);
-        guessesLeft--;
-      }
-    }
-  } else {
-    isWordDone = true;
-    losses++;
-    guessesLeft = 15;
-    getSecretWord();
-  }
+  if (guessesLeft > 0) {
+		indx = logged.indexOf(userInput);
+		if(userInput == logged) {
+			usersArray[indx] = userInput;
+			updateData();
+		} else {
+			guessesLeft--
+			wrongWordArray.push(userInput);
+			updateData();
+		}
+	} else {
+		losses++;
+		updateData();
+	}
 };
 
 function gameWin() {
   if (secretWord === usersArray) {
-    isWordDone = true;
+    //isWordDone = true;
     getSecretWord();
   } else {
-    isWordDone = false;
+    //isWordDone = false;
   }
 };
-
-gameWin();
-
-var winsText = (document.getElementById("wins").innerHTML = wins);
-var lossesText = (document.getElementById("losses").innerHTML = losses);
-var currentWord = (document.getElementById("currentWord").innerHTML = usersArray.join(" "));
-var guessesLeftText = (document.getElementById("guessesLeft").innerHTML = guessesLeft);
-var lettersUsed = (document.getElementById("lettersUsed").innerHTML = wrongWordArray);

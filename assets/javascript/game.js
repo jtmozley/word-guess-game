b39d2f75b0e78204c854417c4d75979632dd372b
JTMozley <jtmozley@gmail.com>
2019-03-23 08:54:09 -0500

    game complete with full functionality. js code cleaned up and organized with notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //names to be used in game
-var characters = ["elsa", "mickey", "ariel", "goofy", "woody"];
+var characters = ["elsa", "mickey", "ariel", "pluto", "jasmine"];
 //word thats selected from characters[]
 var secretWord = "";
 //user's array that stores "_" blanks initially
@@ -12,40 +12,36 @@ var logged = "";
 var indx;
 //key the user pressed
 var userInput = "";
-//var isWordDone = false;
 
 //stats displayed to user
-var wins = 11110;
+var wins = 0;
 var losses = 0;
 var guessesLeft = 0;
 
+//vars with document id's for html printing
 var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 var currentWord = document.getElementById("currentWord");
 var guessesLeftText = document.getElementById("guessesLeft");
 var lettersUsed = document.getElementById("lettersUsed");
 
-//updates any page displayed stats
-function  updateData() {
-	winsText.innerHTML = wins;
-	lossesText.innerHTML = losses;
-	currentWord.innerHTML = usersArray.join(" ");
-	guessesLeftText.innerHTML = guessesLeft;
-	lettersUsed.innerHTML = wrongWordArray;
-};
-
-
 //initializes the game and calles for a new word
 function start() {
   wins = 0;
   losses = 0;
-	guessesLeft = 15;
-	updateData();
-	getSecretWord();
-
+  guessesLeft = 6;
+  updateData();
+  getSecretWord();
 }
 
-start();
+//updates any page displayed stats
+function updateData() {
+  winsText.innerHTML = wins;
+  lossesText.innerHTML = losses;
+  currentWord.innerHTML = usersArray.join(" ");
+  guessesLeftText.innerHTML = guessesLeft;
+  lettersUsed.innerHTML = wrongWordArray;
+}
 
 //select new word, adds to logged, replaces letters with "_" and pushes to usersArray[]
 function getSecretWord() {
@@ -53,41 +49,47 @@ function getSecretWord() {
   logged = secretWord.split("");
   for (var x = 0; x < logged.length; x++) {
     usersArray.push("_");
-	}
-	updateData();
+  }
+  updateData();
   console.log(secretWord);
-  console.log(logged);
-  console.log(usersArray);
-
-  //return usersArray;
 }
-
 
 //records input from user
 document.onkeyup = function(event) {
-	userInput = event.key.toLowerCase();
-	console.log(userInput);
+  userInput = event.key.toLowerCase();
+  console.log(userInput);
   if (guessesLeft > 0) {
-		indx = logged.indexOf(userInput);
-		if(userInput == logged) {
-			usersArray[indx] = userInput;
-			updateData();
-		} else {
-			guessesLeft--
-			wrongWordArray.push(userInput);
-			updateData();
-		}
-	} else {
-		losses++;
-		updateData();
-	}
-};
-
-function gameWin() {
-  if (secretWord === usersArray) {
-    //isWordDone = true;
-    getSecretWord();
+    indx = logged.indexOf(userInput);
+    if (userInput == logged[indx]) {
+      //if correct prints to document, update stats
+      usersArray[indx] = userInput;
+      updateData();
+      gameWin();
+    } else {
+      //incorrect guess logs wrong guess, decrement guessesLeft
+      guessesLeft--;
+      wrongWordArray.push(userInput);
+      updateData();
+    }
   } else {
-    //isWordDone = false;
+    //at loss, increase losses and reset game with new word
+    losses++;
+    guessesLeft = 6;
+    usersArray = [];
+    wrongWordArray = [];
+    updateData();
+    getSecretWord();
   }
 };
+
+//restarts game upon win/correct word guess
+function gameWin() {
+  if (usersArray.toString() == logged.toString()) {
+    wins++;
+    usersArray = [];
+    updateData();
+    getSecretWord();
+  }
+}
+
+start();
